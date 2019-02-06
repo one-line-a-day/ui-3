90b0a1aed29a21b1a9615c9e8f1e4e1b42041bb0
Ian Be <ian84be@gmail.com>
2019-02-06 12:21:13 -0600

    add custom slideshow component

---
+++ assets/js/ianbe.js
@@ -0,0 +1,26 @@
+class SlideShow {
+    constructor(element) {
+        this.element = element;
+        this.slides = document.querySelectorAll('.slides-img');
+        this.leftBtn = document.querySelector('.slides-btn-left');
+        this.leftBtn.addEventListener("click", () => this.cycleLeft());
+        this.rightBtn = document.querySelector('.slides-btn-right');
+        this.rightBtn.addEventListener("click", () => this.cycleRight());
+        this.index=0;
+        this.slides[this.index].classList.add('slides-active');  
+    }
+    cycleRight () {
+        this.slides[this.index].classList.remove('slides-active');  
+        this.index === this.slides.length-1 ? this.index = 0 : this.index ++;
+        this.slides[this.index].classList.add('slides-active');  
+      }
+    cycleLeft() {
+        this.slides[this.index].classList.remove('slides-active');  
+        this.index === 0 ? this.index = this.slides.length-1 : this.index --;
+        this.slides[this.index].classList.add('slides-active');  
+    }
+}
+
+let uxSlideShow = document.querySelectorAll('.slide-show');
+console.log(uxSlideShow);
+uxSlideShow.forEach(el => new SlideShow(el));
