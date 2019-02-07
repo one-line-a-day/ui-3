238817a3376ee1eecf5496ae5b0875365894aa7e
Ian Be <ian84be@gmail.com>
2019-02-07 10:06:01 -0600

    thursday start

--- assets/js/ianbe.js
+++ assets/js/ianbe.js
@@ -3,36 +3,30 @@ class SlideShow {
         this.element = element;
         this.slides = document.querySelectorAll('.slides-img');
         this.leftBtn = document.querySelector('.slides-btn-left');
-        this.leftBtn.addEventListener("click", () => this.cycleLeft());
+        this.leftBtn.addEventListener("click", (ev) => this.cycle(ev));
         this.rightBtn = document.querySelector('.slides-btn-right');
-        this.rightBtn.addEventListener("click", () => this.cycleRight());
+        this.rightBtn.addEventListener("click", (ev) => this.cycle(ev));
         this.index=0;
         // fade in first slide
         this.slides[this.index].classList.add('slides-active');
         TweenMax.to('.slides-active',1, {opacity:1});  
     }
 
-    cycleRight () {
+    cycle(ev) {
         // fade out
         TweenMax.to('.slides-active',0.5, {opacity:0});
         this.slides[this.index].classList.remove('slides-active');
         // load next slide
-        this.index === this.slides.length-1 ? this.index = 0 : this.index ++;
+        if (ev.srcElement.classList.contains('slides-btn-right')) {
+            this.index === this.slides.length-1 ? this.index = 0 : this.index ++;
+        }
+        else if (ev.srcElement.classList.contains('slides-btn-left')) {
+            this.index === 0 ? this.index = this.slides.length-1 : this.index --;
+        }
         this.slides[this.index].classList.add('slides-active');
         // fade in 
-        TweenMax.to('.slides-active',0.5, {opacity:1,delay:0.5});  
+        TweenMax.to('.slides-active',0.5, {opacity:1,delay:0.2});  
       }
-
-    cycleLeft() {
-        // fade out
-        TweenMax.to('.slides-active',0.5, {opacity:0});
-        this.slides[this.index].classList.remove('slides-active');
-        // load next slide
-        this.index === 0 ? this.index = this.slides.length-1 : this.index --;
-        this.slides[this.index].classList.add('slides-active');
-        // fade in 
-        TweenMax.to('.slides-active',0.5, {opacity:1,delay:0.5});  
-    }
 }
 
 let uxSlideShow = document.querySelectorAll('.slide-show');
